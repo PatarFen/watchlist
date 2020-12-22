9fb0093932d76bb75d98ccfb5ab6c35c7ae37006
Peter <fansui1990@gmail.com>
2020-12-22 21:34:31 +0800

    Ready to deploy

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -6,9 +6,10 @@ from flask_sqlalchemy import SQLAlchemy
 #from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dev'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+ os.path.join(os.path.dirname(app.root_path), 'data.db')
+# if the OS is windows, need to change to sqlite:///
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
