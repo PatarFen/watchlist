2635614a4087e987b841086b0e0fb93dc9155d91
Peter <fansui1990@gmail.com>
2021-01-07 20:43:48 +0800

    Add comment function

--- watchlist/views.py
+++ watchlist/views.py
@@ -1,27 +1,45 @@
 from flask import render_template, request, url_for, redirect, flash
 
 from watchlist import app, db
-from watchlist.models import Movie
+from watchlist.models import Movie, Comment
 
 @app.route('/', methods= ['GET', 'POST'])
 def index():
     if request.method == 'POST':
+
+        #This part is about add new movie
         title = request.form.get('title')
         year = request.form.get('year')
-
-        if not title or not year or len(year) > 4 or len(title) > 60:
-            flash('Invalid input.')
-            return redirect(url_for('index'))
         
-        movie = Movie(title = title, year = year)
-        db.session.add(movie)
-        db.session.commit()
-        flash("Item Created")
+        if title and year:
+            if not title or not year or len(year) > 4 or len(title) > 60:
+                flash('Invalid input.')
+                return redirect(url_for('index'))
+            
+            movie = Movie(title = title, year = year)
+            db.session.add(movie)
+            db.session.commit()
+            flash("Item Created")
+
+        #THis part is about add new comment
+        name = request.form.get('name')
+        content = request.form.get('content')
+        
+        if name and content:
+            if not name or not content:
+                flash('Invalid input.')
+                return redirect(url_for('index'))
+            
+            comment = Comment(name = name, content =content)
+            db.session.add(comment)
+            db.session.commit()
+            flash('Comment added')
         
         return redirect(url_for('index'))
 
     movies = Movie.query.all()
-    return render_template('index.html', movies=movies, name = "Peter")
+    comments = Comment.query.all()
+    return render_template('index.html', movies=movies, comments = comments, name = "Peter")
 
 @app.route('/movie/edit/<int:movie_id>', methods = ['GET', 'POST'])
 def edit(movie_id):
